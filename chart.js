e19235b7c18a255d3f6b79e7fe7468d52335534b
Disha2628 <dishadange2603@gmail.com>
2025-03-27 16:19:53 +0530

    Update chart.js

--- chart.js
+++ chart.js
@@ -1,15 +1,10 @@
 async function fetchHistoricalData(crypto, currency) {
     try {
         const URL = `${API_URL}/coins/${crypto}/market_chart?vs_currency=${currency}&days=7`;
-        console.log(URL);
         const response = await fetch(URL);
-        //https://api.coingecko.com/api/v3/coins/
         const data = await response.json();
-        console.log(data);
         const prices = data.prices.map(price => price[1]);
-        console.log(prices);
         const dates = data.prices.map(price => new Date(price[0]).toLocaleDateString());
-        console.log(dates);
         renderChart(dates, prices);
     } catch (error) {
         console.error("Error fetching historical data:", error);
